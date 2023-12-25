43b4e32e83009331a211b6edf9b790a11cbe2970
eyeofsol <kingjihong@naver.com>
2023-12-25 19:52:37 +0900

    9강

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import DayList from './component/DayList';
+import Header from './component/Header';
 
 function App() {
+  console.log("boot");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Header />
+    <DayList />
+  </div>
   );
 }
 
